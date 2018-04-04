ba4d5828e812b558d881d172cb1653927be6a3ba
Martin Wang <martinwangjun@163.com>
2018-04-04 15:13:21 +0800

    leetcode in brute force

---
+++ src/leetcode/0001.js
@@ -0,0 +1,31 @@
+/**
+ * LeetCode Question ID: 1
+ * 
+ * Given an array of integers, return indices of the two numbers such that they 
+ * add up to a specific target. You may assume that each input would have 
+ * exactly one solution, and you may not use the same element twice.
+ * 
+ * @author: martinwangjun@163.com
+ */
+
+const nums = [2, 7, 11, 15, 32, 93, 28]
+const target = 35
+
+// Approach #1: brute force
+let func = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (i !== j) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+}
+console.log(func(nums, target));
+
+
+
+
+
